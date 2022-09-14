2993b7adf02b2074233e3f01c4aa72377385799d
paidabai <1196270151@qq.com>
2022-09-14 20:15:19 +0800

    work

--- work01/js/index.js
+++ work01/js/index.js
@@ -2,6 +2,8 @@ let wear = document.getElementById('wear')
 let popular = document.getElementById('popular')
 let product_popular = document.getElementById('product_popular')
 let product_wear = document.getElementById('product_wear')
+let tool_bar = document.getElementById('tool-bar')
+let gotop = document.getElementById('gotop')
 
 wear.onmouseover = () => {
     wear.classList.add('click')
@@ -16,3 +18,13 @@ popular.onmouseover = () => {
     product_wear.classList.add('vision')
     product_popular.classList.remove('vision')
 }
+
+setInterval(() => {
+    if (window.pageYOffset > 300) {
+        tool_bar.classList.add('gun')
+        gotop.classList.add('go')
+    }else {
+        tool_bar.classList.remove('gun')
+        gotop.classList.remove('go')
+    }
+}, 500);
